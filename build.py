1ef448ebc0db9f8c72e4085bd4ecbf14b39d0e48
Chandler Lofland <chandlerl14@outlook.com>
2020-01-08 16:15:41 -0500

    more build script updates

--- build.py
+++ build.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import os, sys
+import time
 
+start = 0
+end = 0
 count = 0
 command = "openjscad"
 if "linux" in sys.platform:
-    command = "time /usr/local/lib/nodejs/bin/openjscad"
+    command = "/usr/local/lib/nodejs/bin/openjscad"
 
 for file in os.listdir("."):
     if file.endswith(".jscad"):
@@ -14,13 +17,17 @@ for file in os.listdir("."):
                 continue
         print("Building ", file)
         outputname = file.replace(".jscad", ".stl")
+        start = time.time()
         jscad_compile = os.system(f"{command} {file} -of stla")
+        end = time.time()
         if jscad_compile != 0:
             print(f"Build failed on file {file}!")
             sys.exit(1)
         else:
             print("Build succeeded!")
             count += 1
+        t = end-start
+        print(f"Build took {t}")
 
 print(f"Built {count} files successfully!")
 sys.exit(0)
